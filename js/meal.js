963727c2c191f634e26d23e03176e798a9f7aea6
MdAzizulHakim-31 <aazizul924@gmail.com>
2023-04-29 00:49:41 +0600

    Modified

--- js/meal.js
+++ js/meal.js
@@ -22,7 +22,7 @@ const displayMeals = meals => {
             <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
                 Details
             </button>
-
+            <button onclick="window.location.href = 'https://www.foodpanda.com.bd/'" type="button" class="btn btn-primary">Order Food</button>
         </div>
 </div>
 `
@@ -60,12 +60,11 @@ const displayMealDetail = meal => {
     mealDetails.innerHTML = `
     <h5><u>Category:   </u></h5>${meal.strCategory}
     <h5><u>Area:   </u></h5>${meal.strArea}
-    <h5><u>Ingredients:  </u></h5>${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}, ${meal.strIngredient5}, ${meal.strIngredient6}, ${meal.strIngredient7}, ${meal.strIngredient8}, ${meal.strIngredient9}, ${meal.strIngredient10}, ${meal.strIngredient11}, ${meal.strIngredient12}, ${meal.strIngredient13}, ${meal.strIngredient14}, ${meal.strIngredient15}, ${meal.strIngredient16}, ${meal.strIngredient17}, ${meal.strIngredient18}, ${meal.strIngredient19}, ${meal.strIngredient20}
+    <h5><u>Ingredients:  </u></h5>${meal.strIngredient1}_[${meal.strMeasure1}], ${meal.strIngredient2}_[${meal.strMeasure2}], ${meal.strIngredient3}_[${meal.strMeasure3}], ${meal.strIngredient4}_[${meal.strMeasure4}], ${meal.strIngredient5}_[${meal.strMeasure5}], ${meal.strIngredient6}_[${meal.strMeasure6}], ${meal.strIngredient7}_[${meal.strMeasure7}], ${meal.strIngredient8}_[${meal.strMeasure8}], ${meal.strIngredient9}_[${meal.strMeasure9}], ${meal.strIngredient10}_[${meal.strMeasure10}], ${meal.strIngredient11}_[${meal.strMeasure11}], ${meal.strIngredient12}_[${meal.strMeasure12}], ${meal.strIngredient13}_[${meal.strMeasure13}], ${meal.strIngredient14}_[${meal.strMeasure14}], ${meal.strIngredient15}_[${meal.strMeasure15}], ${meal.strIngredient16}_[${meal.strMeasure16}], ${meal.strIngredient17}_[${meal.strMeasure17}], ${meal.strIngredient18}_[${meal.strMeasure18}], ${meal.strIngredient19}_[${meal.strMeasure19}], ${meal.strIngredient20}_[${meal.strMeasure20}]
     <h5><u>Instructions:   </u></h5>${meal.strInstructions}
-    <h5><u>Youtube:   </u></h5><a>(${meal.strYoutube})</a>
+    <h5><u>Youtube:   </u></h5><a>${meal.strYoutube}</a>
     `
 }
-
 
 
 loadMeals('fish');
